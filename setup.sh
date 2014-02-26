e3f65cf2703a83e4071378341e8e634e63517251
Ulysses Grundler <grundler@cern.ch>
2014-02-26 11:56:11 +0100

    Updated paths for new testbeam

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 . /afs/cern.ch/work/g/grundler/public/fnal2013/cmspxltb-analysis/build_env.sh
 
-export jobsub=/afs/cern.ch/cms/Tracker/Pixel/HRbeamtest/jobsub/v1
-export TARGETDIRECTORY=/afs/cern.ch/cms/Tracker/Pixel/HRbeamtest/www/dqm/fnal2013
-export PATH=/afs/cern.ch/cms/Tracker/Pixel/HRbeamtest/dqm/v3/bin:$PATH
+export jobsub=/afs/cern.ch/cms/Tracker/Pixel/HRbeamtest/jobsub/fnal201403
+export TARGETDIRECTORY=/afs/cern.ch/cms/Tracker/Pixel/HRbeamtest/www/dqm/fnal201403
+export PATH=/afs/cern.ch/cms/Tracker/Pixel/HRbeamtest/dqm/fnal201403/bin:$PATH
 
 
