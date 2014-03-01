6bdbe1bce5f8d195d409239eb858f187d649c513
Ulysses Grundler <grundler@cern.ch>
2014-03-01 14:35:23 +0100

    change run directory

--- bash/cron_jobs.sh
+++ bash/cron_jobs.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Usage: 
 # acrontab -e 
-# */5 * * * * lxplus.cern.ch /afs/cern.ch/cms/Tracker/Pixel/HRbeamtest/dqm/v3/bash/cron_jobs.sh
+# */5 * * * * lxplus.cern.ch /afs/cern.ch/cms/Tracker/Pixel/HRbeamtest/dqm/fnal201403/bash/cron_jobs.sh
 
-/afs/cern.ch/cms/Tracker/Pixel/HRbeamtest/dqm/v3/python/dqm.py default 
+/afs/cern.ch/cms/Tracker/Pixel/HRbeamtest/dqm/fnal201403/python/dqm.py default 
 
 #/afs/cern.ch/work/x/xshi/public/dqm/v2/python/ful.py
 
