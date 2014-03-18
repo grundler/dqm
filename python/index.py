fbd3be1c45662f1713592646563e4efe17d354ac
Ulysses Grundler <grundler@cern.ch>
2014-03-18 15:27:59 +0100

    add number of valid events found to index page, and added logging

--- python/index.py
+++ python/index.py
@@ -3,6 +3,14 @@
 import sys
 import os
 import time
+import re
+
+import logging
+FORMAT = '%(asctime)s: %(name)s - %(levelname)s: %(message)s' 
+DATEFMT = '%H:%M:%S'
+logging.basicConfig(format=FORMAT, datefmt=DATEFMT, level=logging.INFO)
+log = logging.getLogger("index")
+
 
 import HTML
 import utils
@@ -11,15 +19,17 @@ from config import *
 def main():
     index()
 
-def index(): 
-    sys.stdout.write('[make index] ... ')
-    sys.stdout.flush()
+def index():
+    log.info('Make index')
+    # sys.stdout.write('[make index] ... ')
+    # sys.stdout.flush()
     procenv = utils.source_bash(dqm_env_file)
     targetdir = procenv['TARGETDIRECTORY']
 
     #runs = []
     run_status = {}
-    
+
+    log.debug('Get job statuses')
     for j in range(JOBS.nJobs):
         for s in range(STATUS.nStatus):
             dbsubdir = os.path.join(dbdir,JOBS.prefix[j],STATUS.prefix[s])
@@ -40,17 +50,34 @@ def index():
 
     # runs = sorted(runs, reverse=True)
 
+    log.debug('Create table')
     header_row = ['Run']
     header_row.extend(JOBS.prefix[:-1])
+    header_row.append('Events valid')
     t = HTML.Table(header_row=header_row)
     for run_board in sorted(run_status, reverse=True):
         run_link = HTML.link(run_board, '%s' %run_board)
         row = [run_link]
 
-        for job in run_status[run_board]:
-            color = STATUS.colors[run_status[run_board][job]]
-            colored_result = HTML.TableCell(STATUS.prefix[run_status[run_board][job]], bgcolor=color)
+        r_b = run_board.split('_')
+        nevts = get_num_events(r_b[0], r_b[1], targetdir)
+
+        # for job in run_status[run_board]:
+        #     color = STATUS.colors[run_status[run_board][job]]
+        #     colored_result = HTML.TableCell(STATUS.prefix[run_status[run_board][job]], bgcolor=color)
+        #     row.append(colored_result)
+
+        for job in range(JOBS.nJobs):
+            if job in run_status[run_board]:
+                color = STATUS.colors[run_status[run_board][job]]
+                colored_result = HTML.TableCell(STATUS.prefix[run_status[run_board][job]], bgcolor=color)
+            else:
+                color = STATUS.colors[STATUS.unknown]
+                colored_result = HTML.TableCell(STATUS.prefix[STATUS.unknown], bgcolor=color)
             row.append(colored_result)
+
+        evtscell = HTML.TableCell(str(nevts), align='right')
+        row.append(evtscell)
         t.rows.append(row)
 
     htmlcode = str(t)
@@ -88,7 +115,8 @@ def index():
     fo.write(htmlcode)
     fo.write(html_footer)
     fo.close()
-    sys.stdout.write(' OK.\n')
+    log.info('Indexing done')
+    # sys.stdout.write(' OK.\n')
 
 def parse_db(dbfile):
     status = STATUS.unknown
@@ -112,6 +140,21 @@ def parse_db(dbfile):
             status = s
     
     return run, board, job, status
+
+def get_num_events(run, board, targetdir):
+    log.debug('Get valid events for run %s', str(run))
+    searchstr = 'Events valid:'
+    f = os.path.join(targetdir,str(run)+'_'+board,'decoding.html')
+    if os.path.isfile(f):
+        with open(f) as decf:
+            for line in decf:
+                if searchstr in line:
+                    log.debug('\t%s', line)
+                    return int(re.findall('\d+', line)[0])
+
+    log.debug('Could not find events')
+    return -1
+
 
 
 if __name__ == '__main__':
